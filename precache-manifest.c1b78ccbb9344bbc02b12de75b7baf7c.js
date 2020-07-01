97de863a45dc8389595e0a34c6eaaeb43a9185b4
Matheus de Alencar Beck <matheus.a.beck@ericssoninovacao.com.br>
2020-07-01 11:56:49 -0300

    Updates

---
+++ precache-manifest.c1b78ccbb9344bbc02b12de75b7baf7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6172109be8b8550692729713a59154",
+    "url": "/react-rocketshoes/index.html"
+  },
+  {
+    "revision": "c1221dd6f02aafa06aee",
+    "url": "/react-rocketshoes/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c1221dd6f02aafa06aee",
+    "url": "/react-rocketshoes/static/js/2.b33d536a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-rocketshoes/static/js/2.b33d536a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60da6646c0d4c512957",
+    "url": "/react-rocketshoes/static/js/main.f22e699a.chunk.js"
+  },
+  {
+    "revision": "a0cd516502aa8b8ee732",
+    "url": "/react-rocketshoes/static/js/runtime-main.3e04e932.js"
+  },
+  {
+    "revision": "018e896037f7c5c627de473e127db7b6",
+    "url": "/react-rocketshoes/static/media/background.018e8960.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/react-rocketshoes/static/media/logo.879557c5.svg"
+  }
+]);
