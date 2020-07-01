c7dcb0794f6bf035d5147ba4a82464166d7c9364
Matheus de Alencar Beck <matheus.a.beck@ericssoninovacao.com.br>
2020-07-01 13:26:08 -0300

    fixed deploy

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,8 @@ import Cart from './pages/Cart';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/cart" component={Cart} />
+      <Route path="/react-rocketshoes/" exact component={Home} />
+      <Route path="/react-rocketshoes/cart" component={Cart} />
     </Switch>
   );
 }
